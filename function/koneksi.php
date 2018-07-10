9f45090303f69aae02e02feb726ad7b768b9fa73
Muhammad Arief Sosiawan <sosiawan.rief@gmail.com>
2018-07-11 01:00:24 +0700

    rm koneksi

--- function/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-	$server = "localhost";
-	$username = "root";
-	$password = "";
-	$database = "eventnesia";
-
-	$koneksi = new PDO("mysql:host=$server;dbname=$database", $username, $password);
